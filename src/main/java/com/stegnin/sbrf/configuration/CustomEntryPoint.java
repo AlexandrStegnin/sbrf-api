c4332487bbe711c35124587f587273f8c69441cf
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-02-06 19:54:09 +0500

    Добавлена конфигурация для сжатого отображения информации, если пользователь не авторизован

---
+++ src/main/java/com/stegnin/sbrf/configuration/CustomEntryPoint.java
@@ -0,0 +1,36 @@
+package com.stegnin.sbrf.configuration;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Objects;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Component
+public class CustomEntryPoint extends BasicAuthenticationEntryPoint {
+
+    @Override
+    public void commence(
+            HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
+            throws IOException {
+        response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName() + "");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        writer.println("HTTP Status 401 - " + Objects.requireNonNull(authEx).getMessage());
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        setRealmName("Stegnin.com");
+        super.afterPropertiesSet();
+    }
+
+}
